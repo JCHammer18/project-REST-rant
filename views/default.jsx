b5429b99d970a919653a6bc5bc6cf1b3728823be
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-12-26 03:25:28 -0500

    Update default.jsx

--- views/default.jsx
+++ views/default.jsx
@@ -1,34 +1,38 @@
+//React import
 const React = require('react')
-
-function Def (html) {
+//Adding default view for layout
+function Def(html) {
     return (
         <html>
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="/style.css" />
             </head>
             <body>
-            <nav>
-  <ul>
-    <li>
-      <a href="/">Home</a>
-    </li>
-    <li>
-      <a href="/places">Places</a>
-    </li>
-    <li>
-      <a href="/places/new">Add Place</a>
-    </li>
-  </ul>
-</nav>
-
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">New Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
+                <div>
+                    {/*BONUS ADDING FOOTER */}
+                    <footer className="footer">
+                        <b>GOOD EAT'S</b>
+                    </footer>
+                </div>
             </body>
         </html>
     )
 }
-
+//Exporting Def function
 module.exports = Def
-
-
