4a0a4dee1edb10e9ddbec406ce0cde48e3df9779
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-12-26 03:25:19 -0500

    Update home.jsx

--- views/home.jsx
+++ views/home.jsx
@@ -1,24 +1,33 @@
+
+//Importing React and Def function
 const React = require('react')
 const Def = require('./default')
-
-function home () {
+//Added link to places
+//Activity Bonus Attempt
+const router = require('./places')
+//Adding home page view
+function home() {
     return (
         <Def>
             <main>
-    <h1>HOME</h1>
-    <div>
-      <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-      <div>
-        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-      </div>
-    </div>
-    <a href="/places">
-        <button className="btn-primary">Places Page</button>
-    </a>
-</main>
-
+                <h1>Rest-Rant</h1>
+                <div>
+                    {/*<Link to="/places">*/}
+                    <a href='/places'>
+                        <img src={"/images/Food-Sampler.jpg"} alt="Chia Fruit Shake" />
+                    </a>
+                    {/*<a href='/places'></a>*/}
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+                </div>
+                <link rel="stylesheet" href="style.css" />
+                <a href='/places'>
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
         </Def>
     )
 }
-
+//code to export from home
 module.exports = home
